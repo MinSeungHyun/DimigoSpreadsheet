c0a157a6f71b9343cfe368cc8c95117878a4b93e
MinSeungHyun <asdf014563@naver.com>
2019-07-10 23:38:42 +0900

    스프레드시트에거 값 불러오기, 저장하기 성공

--- app/src/main/java/com/seunghyun/dimigospreadsheet/MainActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/MainActivity.kt
@@ -4,18 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.api.client.auth.oauth2.Credential
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.sheets.v4.Sheets
-import com.google.api.services.sheets.v4.SheetsScopes
-import java.io.File
-import java.io.InputStreamReader
-import java.util.*
+import com.google.api.services.sheets.v4.model.ValueRange
 
 private const val APPLICATION_NAME = "DimigoSpreadsheet"
 private const val SPREADSHEET_ID = "1l4fEOCp2EahtHnpM44Y7blWf1CUqRLSe2EFVCzXjLQo"
@@ -23,30 +16,33 @@ private const val SPREADSHEET_ID = "1l4fEOCp2EahtHnpM44Y7blWf1CUqRLSe2EFVCzXjLQo
 class MainActivity : AppCompatActivity() {
     private val jsonFactory = JacksonFactory.getDefaultInstance()
     private val httpTransport = NetHttpTransport()
-    private val scopes: List<String> = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY)
+    private val scopes = listOf("https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val service = Sheets.Builder(httpTransport, jsonFactory, getCredentials(httpTransport))
+        val service = Sheets.Builder(httpTransport, jsonFactory, getCredentials())
                 .setApplicationName(APPLICATION_NAME)
                 .build()
 
-        val response = service.spreadsheets().values()
-                .get(SPREADSHEET_ID, "사용 안내!A1:A20")
-                .execute()
-        Log.d("testing", response.getValues().toString())
+        object : Thread() {
+            override fun run() {
+                val response = service.spreadsheets().values()
+                        .get(SPREADSHEET_ID, "test!A1")
+                        .execute()
+                Log.d("testing", response.getValues().toString())
+
+                val request: Sheets.Spreadsheets.Values.Update = service.spreadsheets().values().update(SPREADSHEET_ID, "test!C5:D6", ValueRange().setValues(listOf(listOf("test2", "test3"), listOf("test4", "test5"))))
+                request.valueInputOption = "RAW"
+                Log.d("testing", request.execute().values.toString())
+            }
+        }.start()
+
     }
 
-    private fun getCredentials(httpTransport: NetHttpTransport): Credential {
+    private fun getCredentials(): Credential {
         val inputStream = resources.assets.open("Credentials.json")
-        val clientSecrets = GoogleClientSecrets.load(jsonFactory, InputStreamReader(inputStream))
-        val flow = GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, clientSecrets, scopes)
-                .setDataStoreFactory(FileDataStoreFactory(File("tokens")))
-                .setAccessType("offline")
-                .build()
-        val receiver = LocalServerReceiver.Builder().setPort(8888).build()
-        return AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
+        return GoogleCredential.fromStream(inputStream).createScoped(scopes)
     }
 }
