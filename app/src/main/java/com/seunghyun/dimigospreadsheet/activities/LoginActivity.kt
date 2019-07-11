1b89675ecab98f0ce5b67cadd591251c0c827cfc
MinSeungHyun <asdf014563@naver.com>
2019-07-11 15:21:28 +0900

    로그인 화면 디자인

--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/LoginActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/LoginActivity.kt
@@ -1,15 +1,22 @@
 package com.seunghyun.dimigospreadsheet.activities
 
 import android.os.Bundle
+import android.text.util.Linkify
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.seunghyun.dimigospreadsheet.R
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_login)
 
-
+        val filter = Linkify.TransformFilter { _, _ -> "" }
+        val pattern = Pattern.compile("인원체크 스프레드시트")
+        Linkify.addLinks(loginDescriptionTV, pattern, "http://dimigo18.tk", null, filter)
     }
 }
