e91a80d8a23918ff3d21f8319b939d450977ae2b
MinSeungHyun <asdf014563@naver.com>
2019-07-11 15:30:41 +0900

    로그인 버튼 애니메이션 적용

--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/LoginActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/LoginActivity.kt
@@ -15,8 +15,16 @@ class LoginActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_login)
 
+        //로그인 설명에 링크 걸기
         val filter = Linkify.TransformFilter { _, _ -> "" }
         val pattern = Pattern.compile("인원체크 스프레드시트")
         Linkify.addLinks(loginDescriptionTV, pattern, "http://dimigo18.tk", null, filter)
+
+        //로그인 버튼 클릭 애니메이션
+        loginButton.setOnClickListener {
+            loginButton.startAnimation {
+
+            }
+        }
     }
 }
