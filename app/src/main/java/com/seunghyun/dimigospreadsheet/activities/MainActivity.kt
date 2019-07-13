7bee21844aa0e8f830d8275f1e301f5495ec5885
MinSeungHyun <asdf014563@naver.com>
2019-07-13 22:26:40 +0900

    명단 불러오기 작업 스플래시 화면에서 수행

--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/MainActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.seunghyun.dimigospreadsheet.activities
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.text.util.Linkify
-import android.util.Log
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.google.api.client.auth.oauth2.Credential
@@ -13,86 +13,57 @@ import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.model.ValueRange
 import com.seunghyun.dimigospreadsheet.R
-import com.seunghyun.dimigospreadsheet.models.GetSheetValueCallback
-import com.seunghyun.dimigospreadsheet.utils.JSONParser
-import com.seunghyun.dimigospreadsheet.utils.JWTDecoder
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.regex.Pattern
 
-private const val APPLICATION_NAME = "DimigoSpreadsheet"
-private const val SPREADSHEET_ID = "1l4fEOCp2EahtHnpM44Y7blWf1CUqRLSe2EFVCzXjLQo"
-
 class MainActivity : AppCompatActivity() {
-    private val jsonFactory = JacksonFactory.getDefaultInstance()
-    private val httpTransport = NetHttpTransport()
-    private val scopes = listOf("https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive")
-
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //정보 불러오기
-        val token = JSONParser.parse(intent.getStringExtra("token"), "token")
-        val identity = JSONParser.parse(JWTDecoder.getBody(token), "identity")
-
-        val name = JSONParser.parseFromArray(identity, 0, "name")
-        val studentId = JSONParser.parseFromArray(identity, 0, "serial")
-        val grade = studentId.subSequence(0, 1).toString().toInt()
-        val klass = studentId.subSequence(1, 2).toString().toInt()
-
         //초기화
+        val name = intent.getStringExtra("name")
+        val studentId = intent.getStringExtra("studentId")
+        val grade = intent.getIntExtra("grade", 0)
+        val klass = intent.getIntExtra("class", 0)
+        val names = intent.getStringArrayExtra("names")
+
+        val arrayAdapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_dropdown_item, names)
+        nameSpinner.adapter = arrayAdapter
         nameTV.text = "$studentId $name"
         enterDescriptionTV.text = getString(R.string.enter_description).format(grade, klass)
         val filter = Linkify.TransformFilter { _, _ -> "" }
         val pattern = Pattern.compile(getString(R.string.go_to_sheet))
         Linkify.addLinks(goToSheetButton, pattern, "http://dimigo18.tk", null, filter)
-
-
-        val service = Sheets.Builder(httpTransport, jsonFactory, getCredentials())
-                .setApplicationName(APPLICATION_NAME)
-                .build()
-
-        val getNameCallback = object : GetSheetValueCallback {
-            override fun onReceive(values: List<List<Any>>?) {
-                if (values != null) {
-                    val names = ArrayList<String>()
-                    values.forEach { names.add(it[0].toString()) }
-                    val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_dropdown_item, names)
-                    runOnUiThread {
-                        nameSpinner.adapter = adapter
-                    }
-                } else {
-                    Log.d("testing", "failed")
-                }
-            }
-        }
-
-        object : Thread() {
-            override fun run() {
-                try {
-                    val names = getValues(service, "${klass}반 명단!A:A")
-                    getNameCallback.onReceive(names)
-                } catch (e: Exception) {
-                    getNameCallback.onReceive(null)
-                }
-            }
-        }.start()
-    }
-
-    private fun getCredentials(): Credential {
-        val inputStream = resources.assets.open("Credentials.json")
-        return GoogleCredential.fromStream(inputStream).createScoped(scopes)
     }
 
     companion object {
-        private fun getValues(service: Sheets, range: String): List<List<Any>> {
+        private const val APPLICATION_NAME = "DimigoSpreadsheet"
+        private const val SPREADSHEET_ID = "1l4fEOCp2EahtHnpM44Y7blWf1CUqRLSe2EFVCzXjLQo"
+        private val jsonFactory = JacksonFactory.getDefaultInstance()
+        private val httpTransport = NetHttpTransport()
+        private val scopes = listOf("https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive")
+
+
+        private fun getCredentials(context: Context): Credential {
+            val inputStream = context.resources.assets.open("Credentials.json")
+            return GoogleCredential.fromStream(inputStream).createScoped(scopes)
+        }
+
+        fun getService(context: Context): Sheets {
+            return Sheets.Builder(httpTransport, jsonFactory, getCredentials(context))
+                    .setApplicationName(APPLICATION_NAME)
+                    .build()
+        }
+
+        fun getValues(service: Sheets, range: String): List<List<Any>> {
             return service.spreadsheets().values()
                     .get(SPREADSHEET_ID, range)
                     .execute().getValues()
         }
 
-        private fun updateValues(service: Sheets, range: String, values: ValueRange): MutableCollection<Any> {
+        fun updateValues(service: Sheets, range: String, values: ValueRange): MutableCollection<Any> {
             service.spreadsheets().values().update(SPREADSHEET_ID, range, values).apply {
                 valueInputOption = "RAW"
                 return execute().values
--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/SplashActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/SplashActivity.kt
@@ -7,11 +7,15 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import com.seunghyun.dimigospreadsheet.R
+import com.seunghyun.dimigospreadsheet.models.GetSheetValueCallback
 import com.seunghyun.dimigospreadsheet.models.Result
 import com.seunghyun.dimigospreadsheet.models.ServerCallback
+import com.seunghyun.dimigospreadsheet.utils.JSONParser
+import com.seunghyun.dimigospreadsheet.utils.JWTDecoder
 import com.seunghyun.dimigospreadsheet.utils.ServerRequest
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -22,10 +26,49 @@ class SplashActivity : AppCompatActivity() {
                 runOnUiThread {
                     loadingTV.setText(R.string.loading)
                 }
+
+                val token = JSONParser.parse(result.content!!, "token")
+                val identity = JSONParser.parse(JWTDecoder.getBody(token), "identity")
+                val name = JSONParser.parseFromArray(identity, 0, "name")
+                val studentId = JSONParser.parseFromArray(identity, 0, "serial")
+                val grade = studentId.subSequence(0, 1).toString().toInt()
+                val klass = studentId.subSequence(1, 2).toString().toInt()
+
                 val intent = Intent(this@SplashActivity, MainActivity::class.java).apply {
-                    putExtra("token", result.content)
+                    putExtra("name", name)
+                    putExtra("studentId", studentId)
+                    putExtra("grade", grade)
+                    putExtra("class", klass)
                 }
-                Handler(Looper.getMainLooper()).postDelayed(DelayHandler(intent, this@SplashActivity), 1000)
+
+                val getNameCallback = object : GetSheetValueCallback {
+                    override fun onReceive(values: List<List<Any>>?) {
+                        if (values != null) {
+                            val names = ArrayList<String>()
+                            values.forEach { names.add(it[0].toString()) }
+                            intent.putExtra("names", names.toTypedArray())
+
+                            Handler(Looper.getMainLooper()).postDelayed(DelayHandler(intent, this@SplashActivity), 0)
+                        } else {
+                            runOnUiThread {
+                                Toast.makeText(this@SplashActivity, R.string.loading_failed, Toast.LENGTH_LONG).show()
+                            }
+                            Handler(Looper.getMainLooper()).postDelayed(DelayHandler(Intent(this@SplashActivity, LoginActivity::class.java), this@SplashActivity), 0)
+                        }
+                    }
+                }
+
+                object : Thread() {
+                    override fun run() {
+                        try {
+                            val service = MainActivity.getService(applicationContext)
+                            val names = MainActivity.getValues(service, "${klass}반 명단!A:A")
+                            getNameCallback.onReceive(names)
+                        } catch (e: Exception) {
+                            getNameCallback.onReceive(null)
+                        }
+                    }
+                }.start()
             } else {
                 runOnUiThread {
                     loadingTV.setText(R.string.login_failed)
