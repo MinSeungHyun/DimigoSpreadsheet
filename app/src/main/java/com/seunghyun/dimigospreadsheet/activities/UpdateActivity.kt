53ba4b76e4b3dbc11bac24db656ff2a90b691e38
MinSeungHyun <asdf014563@naver.com>
2019-07-14 15:34:37 +0900

    인앱 업데이트 구현

--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/UpdateActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/UpdateActivity.kt
@@ -1,5 +1,6 @@
 package com.seunghyun.dimigospreadsheet.activities
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
@@ -9,6 +10,10 @@ import android.util.Log
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.seunghyun.dimigospreadsheet.R
 import kotlinx.android.synthetic.main.activity_update.*
 
@@ -18,8 +23,15 @@ class UpdateActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_update)
 
+        val updateInfo = getAppUpdateInfo(this@UpdateActivity)
+        val inAppUpdateAvailability = updateInfo != null && isUpdateAvailable(updateInfo)
+
         updateButton.setOnClickListener {
-            openStore(this@UpdateActivity)
+            if (inAppUpdateAvailability) {
+                startUpdate(this@UpdateActivity, updateInfo!!)
+            } else {
+                openStore(this@UpdateActivity)
+            }
         }
     }
 
@@ -32,5 +44,31 @@ class UpdateActivity : AppCompatActivity() {
         } catch (e: ActivityNotFoundException) {
             Toast.makeText(context, context.getString(R.string.store_not_found), Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun getAppUpdateInfo(context: Context): AppUpdateInfo? {
+        val appUpdateManager = AppUpdateManagerFactory.create(context)
+        val appUpdateInfo = appUpdateManager.appUpdateInfo
+        var updateInfo: AppUpdateInfo? = null
+        appUpdateInfo.addOnCompleteListener {
+            if (it.isComplete) {
+                try {
+                    updateInfo = it.result
+                } catch (e: Exception) {
+                    return@addOnCompleteListener
+                }
+            }
+        }
+        return updateInfo
+    }
+
+    private fun isUpdateAvailable(updateInfo: AppUpdateInfo): Boolean {
+        return (updateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && updateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE))
+    }
+
+    private fun startUpdate(activity: Activity, updateInfo: AppUpdateInfo) {
+        val appUpdateManager = AppUpdateManagerFactory.create(activity)
+        appUpdateManager.startUpdateFlowForResult(updateInfo, AppUpdateType.IMMEDIATE, activity, 1)
     }
 }
