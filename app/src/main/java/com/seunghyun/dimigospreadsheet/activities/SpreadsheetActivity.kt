600259b1a73d51e68c26644e2f09ab215925acdc
MinSeungHyun <asdf014563@naver.com>
2019-07-17 22:42:03 +0900

    스프레드시트 1초마다 불러오기 성공

--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/SpreadsheetActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/SpreadsheetActivity.kt
@@ -1,19 +1,56 @@
 package com.seunghyun.dimigospreadsheet.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import com.seunghyun.dimigospreadsheet.R
+import kotlinx.android.synthetic.main.activity_spreadsheet.*
 
 class SpreadsheetActivity : AppCompatActivity() {
+    private var isRunning = false
+    private var isShowing = false
+    private val grade by lazy { intent.getIntExtra("grade", 0) }
+    private val klass by lazy { intent.getIntExtra("class", 0) }
+    private val service by lazy { MainActivity.getService(this@SpreadsheetActivity) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spreadsheet)
-        val grade = intent.getIntExtra("grade", 0)
-        val klass = intent.getIntExtra("class", 0)
+        isRunning = true
         title = "${grade}학년 ${klass}반"
+
+        object : Thread() {
+            override fun run() {
+                while (isRunning) {
+                    if (isShowing) {
+                        val result = MainActivity.getValues(service, "${klass}반!1:30")
+                        Log.d("testing", result.toString())
+                        runOnUiThread {
+                            testTV.text = result.toString()
+                        }
+                    }
+                    sleep(1000)
+                }
+            }
+        }.start()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isShowing = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isShowing = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isRunning = false
     }
 
     override fun onBackPressed() {
