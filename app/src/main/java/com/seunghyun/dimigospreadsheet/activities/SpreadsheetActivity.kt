52dbcfc7491ee9411c813ddbe58456f0196460bf
MinSeungHyun <asdf014563@naver.com>
2019-07-20 10:55:08 +0900

    updateSheetValues() 추가

--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/SpreadsheetActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/SpreadsheetActivity.kt
@@ -11,6 +11,7 @@ import com.seunghyun.dimigospreadsheet.R
 import com.seunghyun.dimigospreadsheet.models.SheetValue
 import kotlinx.android.synthetic.main.activity_spreadsheet.*
 import kotlinx.android.synthetic.main.number_card_prototype.view.*
+import java.lang.Thread.sleep
 
 class SpreadsheetActivity : AppCompatActivity() {
     private var isRunning = false
@@ -34,42 +35,8 @@ class SpreadsheetActivity : AppCompatActivity() {
         bathroomLayout.typeTV.setText(R.string.bathroom)
 
         object : Thread() {
-            @SuppressLint("SetTextI18n")
             override fun run() {
-                var oldIngang1: ArrayList<String>? = null
-                var oldIngang2: ArrayList<String>? = null
-                var oldClub: ArrayList<String>? = null
-                var oldEtc: ArrayList<String>? = null
-                var oldBathroom: ArrayList<String>? = null
-                while (isRunning) {
-                    if (isShowing) {
-                        try {
-                            val sheetValue = SheetValue(MainActivity.getValues(service, "${klass}반!1:30"))
-                            runOnUiThread {
-                                checkInternetLayout.visibility = View.GONE
-                                totalTV.text = getString(R.string.total) + sheetValue.totalCount
-                                vacancyTV.text = getString(R.string.vacancy) + sheetValue.vacancyCount
-                                currentTV.text = getString(R.string.current) + sheetValue.currentCount
-                                if (oldIngang1 == null || !isSameValues(oldIngang1!!, sheetValue.ingang1)) enterListToParent(ingang1Layout.namesLayout, sheetValue.ingang1)
-                                if (oldIngang2 == null || !isSameValues(oldIngang2!!, sheetValue.ingang2)) enterListToParent(ingang2Layout.namesLayout, sheetValue.ingang2)
-                                if (oldClub == null || !isSameValues(oldClub!!, sheetValue.club)) enterListToParent(clubLayout.namesLayout, sheetValue.club)
-                                if (oldEtc == null || !isSameValues(oldEtc!!, sheetValue.etc)) enterListToParent(etcLayout.namesLayout, sheetValue.etc)
-                                if (oldBathroom == null || !isSameValues(oldBathroom!!, sheetValue.bathroom)) enterListToParent(bathroomLayout.namesLayout, sheetValue.bathroom)
-
-                                oldIngang1 = sheetValue.ingang1
-                                oldIngang2 = sheetValue.ingang2
-                                oldClub = sheetValue.club
-                                oldEtc = sheetValue.etc
-                                oldBathroom = sheetValue.bathroom
-                            }
-                        } catch (e: Exception) {
-                            runOnUiThread {
-                                checkInternetLayout.visibility = View.VISIBLE
-                            }
-                        }
-                    }
-                    sleep(1000)
-                }
+                updateSheetValues()
             }
         }.start()
     }
@@ -105,5 +72,43 @@ class SpreadsheetActivity : AppCompatActivity() {
 
     private fun isSameValues(list1: ArrayList<String>, list2: ArrayList<String>): Boolean {
         return list1.size == list2.size && list1.containsAll(list2)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateSheetValues() {
+        var oldIngang1: ArrayList<String>? = null
+        var oldIngang2: ArrayList<String>? = null
+        var oldClub: ArrayList<String>? = null
+        var oldEtc: ArrayList<String>? = null
+        var oldBathroom: ArrayList<String>? = null
+        while (isRunning) {
+            if (isShowing) {
+                try {
+                    val sheetValue = SheetValue(MainActivity.getValues(service, "${klass}반!1:30"))
+                    runOnUiThread {
+                        checkInternetLayout.visibility = View.GONE
+                        totalTV.text = getString(R.string.total) + sheetValue.totalCount
+                        vacancyTV.text = getString(R.string.vacancy) + sheetValue.vacancyCount
+                        currentTV.text = getString(R.string.current) + sheetValue.currentCount
+                        if (oldIngang1 == null || !isSameValues(oldIngang1!!, sheetValue.ingang1)) enterListToParent(ingang1Layout.namesLayout, sheetValue.ingang1)
+                        if (oldIngang2 == null || !isSameValues(oldIngang2!!, sheetValue.ingang2)) enterListToParent(ingang2Layout.namesLayout, sheetValue.ingang2)
+                        if (oldClub == null || !isSameValues(oldClub!!, sheetValue.club)) enterListToParent(clubLayout.namesLayout, sheetValue.club)
+                        if (oldEtc == null || !isSameValues(oldEtc!!, sheetValue.etc)) enterListToParent(etcLayout.namesLayout, sheetValue.etc)
+                        if (oldBathroom == null || !isSameValues(oldBathroom!!, sheetValue.bathroom)) enterListToParent(bathroomLayout.namesLayout, sheetValue.bathroom)
+
+                        oldIngang1 = sheetValue.ingang1
+                        oldIngang2 = sheetValue.ingang2
+                        oldClub = sheetValue.club
+                        oldEtc = sheetValue.etc
+                        oldBathroom = sheetValue.bathroom
+                    }
+                } catch (e: Exception) {
+                    runOnUiThread {
+                        checkInternetLayout.visibility = View.VISIBLE
+                    }
+                }
+            }
+            sleep(1000)
+        }
     }
 }
