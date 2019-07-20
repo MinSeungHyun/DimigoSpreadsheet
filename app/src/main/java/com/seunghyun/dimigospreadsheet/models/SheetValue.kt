7aa3b1a0fec27898b3bc0c789da0bbadfcad6e04
MinSeungHyun <asdf014563@naver.com>
2019-07-20 17:47:30 +0900

    인원수에 값이 없으면 발생하던 오류 해결

--- app/src/main/java/com/seunghyun/dimigospreadsheet/models/SheetValue.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/models/SheetValue.kt
@@ -1,15 +1,23 @@
 package com.seunghyun.dimigospreadsheet.models
 
 class SheetValue(private val sheetValues: List<List<Any>>?) {
-    val totalCount by lazy { sheetValues?.get(1)?.get(1).toString() }
-    val vacancyCount by lazy { sheetValues?.get(2)?.get(1).toString() }
-    val currentCount by lazy { sheetValues?.get(3)?.get(1).toString() }
+    val totalCount by lazy { getCount(1) }
+    val vacancyCount by lazy { getCount(2) }
+    val currentCount by lazy { getCount(3) }
 
     val ingang1 by lazy { getColumnValues(2, 1) }
     val ingang2 by lazy { getColumnValues(3, 1) }
     val club by lazy { getColumnValues(4, 1) }
     val etc by lazy { getColumnValues(5, 1) }
     val bathroom by lazy { getColumnValues(0, 9) }
+
+    private fun getCount(position: Int): String {
+        return try {
+            sheetValues?.get(position)?.get(1).toString()
+        } catch (e: Exception) {
+            "0"
+        }
+    }
 
     private fun getColumnValues(column: Int, minColumn: Int): ArrayList<String> {
         val values = ArrayList<String>()
