5dd2ff9fbac073d3a6509723e3046dedbc3521e5
MinSeungHyun <asdf014563@naver.com>
2019-07-22 22:15:58 +0900

    TeacherSpreadsheetActivity 디자인 생성

--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/TeacherSpreadsheetActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/TeacherSpreadsheetActivity.kt
@@ -3,11 +3,14 @@ package com.seunghyun.dimigospreadsheet.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.seunghyun.dimigospreadsheet.R
+import kotlinx.android.synthetic.main.activity_teacher_spreadsheet.*
 
 class TeacherSpreadsheetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teacher_spreadsheet)
+        title = ""
+        titleTV.text = "1학년 1반"
     }
 }
