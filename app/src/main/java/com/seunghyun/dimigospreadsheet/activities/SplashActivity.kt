73a8c28f6cd86f386ef9e685d01766e393eeb3cd
MinSeungHyun <asdf014563@naver.com>
2019-07-22 19:47:59 +0900

    선생님이 로그인했을 때 SplashActivity에서 전달하는 intent 변경

--- app/src/main/java/com/seunghyun/dimigospreadsheet/activities/SplashActivity.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/activities/SplashActivity.kt
@@ -25,59 +25,75 @@ class SplashActivity : AppCompatActivity() {
     private val loginCallback = object : ServerCallback {
         override fun onReceive(result: Result) {
             if (result.code == 200) {
-                runOnUiThread {
-                    loadingTV.setText(R.string.loading)
-                }
-
                 val token = JSONParser.parse(result.content!!, "token")
                 val identity = JSONParser.parse(JWTDecoder.getBody(token), "identity")
+                val userType = JSONParser.parseFromArray(identity, 0, "user_type")
                 val name = JSONParser.parseFromArray(identity, 0, "name")
-                val studentId = JSONParser.parseFromArray(identity, 0, "serial")
-                val grade = studentId.subSequence(0, 1).toString().toInt()
-                val klass = studentId.subSequence(1, 2).toString().toInt()
 
-                val intent = Intent(this@SplashActivity, SpreadsheetActivity::class.java).apply {
-                    putExtra("name", name)
-                    putExtra("grade", grade)
-                    putExtra("class", klass)
-                }
+                val intent: Intent
+                if (userType == "S") {
+                    val studentId = JSONParser.parseFromArray(identity, 0, "serial")
+                    val grade = studentId.subSequence(0, 1).toString().toInt()
+                    val klass = studentId.subSequence(1, 2).toString().toInt()
 
-                val getNameCallback = object : GetSheetValueCallback {
-                    override fun onReceive(values: List<List<Any>>) {
-                        if (values[0][0] != "error") {
-                            val names = ArrayList<String>()
-                            values.forEach { if (it.isNotEmpty()) names.add(it[0].toString()) }
-                            intent.putExtra("names", names.toTypedArray())
+                    runOnUiThread {
+                        loadingTV.text = getString(R.string.welcome).format(name)
+                    }
 
-                            Handler(Looper.getMainLooper()).postDelayed(DelayHandler(intent, this@SplashActivity), 0)
-                        } else {
-                            runOnUiThread {
-                                Toast.makeText(this@SplashActivity, values[0][1].toString(), Toast.LENGTH_LONG).show()
+                    intent = Intent(this@SplashActivity, SpreadsheetActivity::class.java).apply {
+                        putExtra("userType", userType)
+                        putExtra("name", name)
+                        putExtra("grade", grade)
+                        putExtra("class", klass)
+                    }
+
+                    val getNameCallback = object : GetSheetValueCallback {
+                        override fun onReceive(values: List<List<Any>>) {
+                            if (values[0][0] != "error") {
+                                val names = ArrayList<String>()
+                                values.forEach { if (it.isNotEmpty()) names.add(it[0].toString()) }
+                                intent.putExtra("names", names.toTypedArray())
+
+                                Handler(Looper.getMainLooper()).postDelayed(DelayHandler(intent, this@SplashActivity), 0)
+                            } else {
+                                runOnUiThread {
+                                    Toast.makeText(this@SplashActivity, values[0][1].toString(), Toast.LENGTH_LONG).show()
+                                }
+                                Handler(Looper.getMainLooper()).postDelayed(DelayHandler(Intent(this@SplashActivity, LoginActivity::class.java), this@SplashActivity), 0)
                             }
-                            Handler(Looper.getMainLooper()).postDelayed(DelayHandler(Intent(this@SplashActivity, LoginActivity::class.java), this@SplashActivity), 0)
                         }
                     }
+
+                    object : Thread() {
+                        override fun run() {
+                            try {
+                                val service = SpreadsheetHelper.getService(applicationContext)
+                                var names = SpreadsheetHelper.getValues(service, "${klass}반 명단!A:A")
+                                if (names == null) names = ArrayList(listOf(ArrayList(listOf("error", getString(R.string.names_not_found))))).toList()
+                                getNameCallback.onReceive(names)
+
+                            } catch (e: GoogleJsonResponseException) {
+                                e.printStackTrace()
+                                val names = ArrayList(listOf(ArrayList(listOf("error", getString(R.string.server_error))))).toList()
+                                getNameCallback.onReceive(names)
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                                val names = ArrayList(listOf(ArrayList(listOf("error", getString(R.string.check_internet))))).toList()
+                                getNameCallback.onReceive(names)
+                            }
+                        }
+                    }.start()
+                } else {
+                    runOnUiThread {
+                        loadingTV.text = getString(R.string.welcome_teacher).format(name)
+                    }
+
+                    intent = Intent(this@SplashActivity, SpreadsheetActivity::class.java).apply {
+                        putExtra("userType", userType)
+                        putExtra("name", name)
+                    }
+                    Handler(Looper.getMainLooper()).postDelayed(DelayHandler(intent, this@SplashActivity), 1000)
                 }
-
-                object : Thread() {
-                    override fun run() {
-                        try {
-                            val service = SpreadsheetHelper.getService(applicationContext)
-                            var names = SpreadsheetHelper.getValues(service, "${klass}반 명단!A:A")
-                            if (names == null) names = ArrayList(listOf(ArrayList(listOf("error", getString(R.string.names_not_found))))).toList()
-                            getNameCallback.onReceive(names)
-
-                        } catch (e: GoogleJsonResponseException) {
-                            e.printStackTrace()
-                            val names = ArrayList(listOf(ArrayList(listOf("error", getString(R.string.server_error))))).toList()
-                            getNameCallback.onReceive(names)
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            val names = ArrayList(listOf(ArrayList(listOf("error", getString(R.string.check_internet))))).toList()
-                            getNameCallback.onReceive(names)
-                        }
-                    }
-                }.start()
             } else {
                 runOnUiThread {
                     loadingTV.setText(R.string.login_failed)
