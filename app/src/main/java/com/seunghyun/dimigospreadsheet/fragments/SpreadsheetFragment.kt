36f0f28d8504c1edb74a03d76defa7baf0594eb9
MinSeungHyun <asdf014563@naver.com>
2019-07-23 13:38:41 +0900

    레이아웃 변경

--- app/src/main/java/com/seunghyun/dimigospreadsheet/fragments/SpreadsheetFragment.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/fragments/SpreadsheetFragment.kt
@@ -9,6 +9,6 @@ import com.seunghyun.dimigospreadsheet.R
 
 class SpreadsheetFragment(private val klass: Int) : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.activity_spreadsheet, container, false)
+        return inflater.inflate(R.layout.spreadsheet_prototype, container, false)
     }
 }
