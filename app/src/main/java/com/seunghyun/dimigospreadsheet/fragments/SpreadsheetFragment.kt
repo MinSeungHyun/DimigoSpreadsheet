1da780dc84530312207513da6f0c9d062010e4db
MinSeungHyun <asdf014563@naver.com>
2019-07-23 18:08:07 +0900

    ViewModel 초기화

--- app/src/main/java/com/seunghyun/dimigospreadsheet/fragments/SpreadsheetFragment.kt
+++ app/src/main/java/com/seunghyun/dimigospreadsheet/fragments/SpreadsheetFragment.kt
@@ -5,10 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.seunghyun.dimigospreadsheet.R
+import com.seunghyun.dimigospreadsheet.models.SheetViewModel
+import com.seunghyun.dimigospreadsheet.utils.SpreadsheetHelper
+import com.seunghyun.dimigospreadsheet.utils.ViewModelFactory
 
 class SpreadsheetFragment(private val klass: Int) : Fragment() {
+    private val viewModel by lazy {
+        ViewModelProviders.of(this, ViewModelFactory(SpreadsheetHelper.getService(requireContext()), klass))[klass.toString(), SheetViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.isRunning.value = true
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.spreadsheet_prototype, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.isShowing.value = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.isShowing.value = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.isRunning.value = false
     }
 }
